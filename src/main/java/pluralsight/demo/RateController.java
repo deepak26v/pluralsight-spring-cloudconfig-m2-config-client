a58e4df5bd69b0ee8361dc7c438b4785bbb8fe9a
VALECD3 <deepak26v@gmail.com>
2017-04-26 12:25:18 -0400

    Commits

--- src/main/java/pluralsight/demo/RateController.java
+++ src/main/java/pluralsight/demo/RateController.java
@@ -1,12 +1,14 @@
 package pluralsight.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RefreshScope
 public class RateController {
 
     @Value("${rate}")
@@ -18,11 +20,15 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
+    @Value("${connstring}")
+    String connstring;
+
     @RequestMapping(value = "/rate", method = RequestMethod.GET)
     public String getRate(Model m) {
         m.addAttribute("rateamount", rateamount);
         m.addAttribute("lanes", lanes);
         m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring", connstring);
 
         //return the view name
         return "rateview";
